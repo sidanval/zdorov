e465cd441435f868168d5aa6f0a4c40a90dd5922
sidan <sidan@sidan.sidan>
2016-09-11 17:45:48 +0300

    fix left menu

--- backend/views/layouts/content.php
+++ backend/views/layouts/content.php
@@ -24,6 +24,7 @@ use dmstr\widgets\Alert;
         <?=
         Breadcrumbs::widget(
             [
+                'homeLink' => false,
                 'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
             ]
         ) ?>
--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -12,10 +12,21 @@
                         'label' => 'Пользователи',
                         'url' => ['user/index'],
                         'icon' => 'fa fa-user',
-                        'visible' => !Yii::$app->user->isGuest && Yii::$app->user->getIdentity()->isAdmin
+                        'visible' => !Yii::$app->user->isGuest && Yii::$app->user->getIdentity()->isAdmin,
+                        'active' => Yii::$app->controller->id == 'user'
                     ],
-                    ['label' => 'Заказы', 'icon' => 'fa fa-ticket', 'url' => ['purchase/index']],
-                    ['label' => 'История', 'icon' => 'fa fa-history', 'url' => ['history/index']],
+                    [
+                        'label' => 'Заказы',
+                        'icon' => 'fa fa-ticket',
+                        'url' => ['purchase/index'],
+                        'active' => Yii::$app->controller->id == 'purchase'
+                    ],
+                    [
+                        'label' => 'История',
+                        'icon' => 'fa fa-history',
+                        'url' => ['history/index'],
+                        'active' => Yii::$app->controller->id == 'history'
+                    ],
                     ['label' => 'Gii', 'icon' => 'fa fa-file-code-o', 'url' => ['/gii'], 'visible' => YII_DEBUG],
                     ['label' => 'Debug', 'icon' => 'fa fa-dashboard', 'url' => ['/debug'], 'visible' => YII_DEBUG],
                 ],
